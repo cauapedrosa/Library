0d4217b4ca9f31a78f03fa72e09786baf16e2148
Cauã Pedrosa <cauapedrosa@live.com>
2016-07-02 15:56:24 -0300

    Updated Library

--- src/model/Library.java
+++ src/model/Library.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Library {
+	private Item[] items;
+
+	public Library() {
+	}
+
+	public Item get(String name) {
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].getName().equals(name)) {
+				return items[i];
+			}
+		}
+		return null;
+	}
+
+	public void add(String name) {
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] == null) {
+				items[i] = name;
+				break;
+			}
+		}
+	}
 
 }
