71617cbf317e6e11d02ed4b2d3f1b1d718a3d312
Letícia do Nascimento <leticia-nascimento@outlook.com>
2016-07-03 21:51:11 -0300

    Update Book.java

--- src/model/Book.java
+++ src/model/Book.java
@@ -2,13 +2,20 @@ package model;
 
 public class Book extends Item {
 
-	private final int releaseYear;
-        private final String author;
+    private final int releaseYear;
+    private final String author;
 
-	public Book(String name, int quantityOwned, String author, int releaseYear) {
-		super(name, quantityOwned);
-		this.releaseYear = releaseYear;
-                this.author = author;
-	}
+    public Book(String name, int quantityOwned, String author, int releaseYear) {
+        super(name, quantityOwned);
+        this.releaseYear = releaseYear;
+        this.author = author;
+    }
 
+    public int getRelease() {
+        return releaseYear;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
